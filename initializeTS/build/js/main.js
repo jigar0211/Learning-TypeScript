32933720ee69a53eca4038ba68317b7693e5aa1b
jigar <jigar.k@simformsolutions.com>
2023-03-16 10:44:32 +0530

    file added

---
+++ initializeTS/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+var user = "jigar";
+console.log(user);
+let a = 12;
+let b = 6;
+let c = 4;
+console.log(a / b);
+console.log(c * b);
---
+++ initializeTS/src/main.ts
@@ -0,0 +1,9 @@
+var user = "jigar"
+console.log(user)
+
+let a = 12
+let b = 6
+let c = 4
+
+console.log(a / b)
+console.log(c * b)
